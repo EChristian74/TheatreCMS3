7b2a840f5c6d2712ad1d3233918029aa45a86326
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2022-01-13 19:38:03 -0600

    Initial Commit

---
+++ Controllers/RentalRequestsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TheatreCMS3.Areas.Rent.Models;
+using TheatreCMS3.Models;
+
+namespace TheatreCMS3.Areas.Rent.Controllers
+{
+    public class RentalRequestsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Rent/RentalRequests
+        public ActionResult Index()
+        {
+            return View(db.RentalRequests.ToList());
+        }
+
+        // GET: Rent/RentalRequests/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RentalRequest rentalRequest = db.RentalRequests.Find(id);
+            if (rentalRequest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rentalRequest);
+        }
+
+        // GET: Rent/RentalRequests/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rent/RentalRequests/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RentalRequestID,ContactPerson,Company,RequestedTime,StartTime,EndTime,ProjectInfo,RentalCode,Accepted,ContractSigned")] RentalRequest rentalRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RentalRequests.Add(rentalRequest);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rentalRequest);
+        }
+
+        // GET: Rent/RentalRequests/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RentalRequest rentalRequest = db.RentalRequests.Find(id);
+            if (rentalRequest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rentalRequest);
+        }
+
+        // POST: Rent/RentalRequests/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RentalRequestID,ContactPerson,Company,RequestedTime,StartTime,EndTime,ProjectInfo,RentalCode,Accepted,ContractSigned")] RentalRequest rentalRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rentalRequest).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rentalRequest);
+        }
+
+        // GET: Rent/RentalRequests/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RentalRequest rentalRequest = db.RentalRequests.Find(id);
+            if (rentalRequest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rentalRequest);
+        }
+
+        // POST: Rent/RentalRequests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            RentalRequest rentalRequest = db.RentalRequests.Find(id);
+            db.RentalRequests.Remove(rentalRequest);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/RentalRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Rent.Models
+{
+    public class RentalRequest
+    {
+        public int RentalRequestID { get; set; }
+        public string ContactPerson { get; set; }
+        public string Company { get; set; }
+        public DateTime RequestedTime { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string ProjectInfo { get; set; }
+        public int RentalCode { get; set; }
+        public bool Accepted { get; set; }
+        public bool ContractSigned { get; set; }
+    }
+}
